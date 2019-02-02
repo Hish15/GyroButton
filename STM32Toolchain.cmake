a98108d94107634270282cb9af60a0f953a2a669
Hish15 <pharamhector@gmail.com>
2019-02-02 19:56:36 +0100

    Working version of Cmake with Toolchain file

--- STM32Toolchain.cmake
+++ STM32Toolchain.cmake
@@ -6,15 +6,14 @@ set(MCU_FLOAT_ABI hard)
 set(MCU_FPU fpv4-sp-d16)
 
 set(CMAKE_SYSTEM_NAME Generic)
-set(CMAKE_SYSTEM_VERSION 1)
 
 set(CMAKE_C_COMPILER 	"/opt/gcc-arm-none-eabi-8-2018-q4-major/bin/arm-none-eabi-gcc")
 set(CMAKE_ASM_COMPILER 	"/opt/gcc-arm-none-eabi-8-2018-q4-major/bin/arm-none-eabi-gcc")
 set(CMAKE_CXX_COMPILER 	"/opt/gcc-arm-none-eabi-8-2018-q4-major/bin/arm-none-eabi-g++")
 
 
-set(STM32F4_FLAGS "-mcpu=cortex-m3 -mthumb -mno-thumb-interwork -mfpu=vfp -msoft-float -mfix-cortex-m3-ldrd")
+set(STM32F4_FLAGS "-mcpu=cortex-m4 -mthumb -mno-thumb-interwork -mfpu=vfp -msoft-float -mfix-cortex-m4-ldrd")
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall ${STM32F4_FLAGS}")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall --std=c++14 ${STM32F4_FLAGS}")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall --std=c++11 ${STM32F4_FLAGS}")
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --specs=rdimon.specs")
